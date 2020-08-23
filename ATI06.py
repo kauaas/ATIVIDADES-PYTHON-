f518fdf412fedd42dcefcc1329e2d337f428a121
kauaas <57207681+kauaas@users.noreply.github.com>
2020-08-23 14:28:40 -0300

    Add files via upload

---
+++ ATI06.py
@@ -0,0 +1,7 @@
+ 
+##Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
+pre=float(input('Quanto ganha por hora? '))
+hrs=float(input('Quantas horas que você trabalhou ao mês? '))
+salario= pre*hrs
+
+print('O preço da sua hora é {} \n O números de horas trabalhadas foi de {} \n O salário do mês foi de {}'.format(pre,hrs,salario))
