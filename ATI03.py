6224f4032ca5aa5baa81d4a003c370d6f7d2f41d
kauaas <57207681+kauaas@users.noreply.github.com>
2020-08-23 14:20:05 -0300

    Add files via upload

---
+++ ATI03.py
@@ -0,0 +1,6 @@
+##Faça um Programa que peça dois números e imprima a soma.
+n1 = float(input('Informe o primeiro número: '))
+n2 =  float(input('Informe o segundo número: '))
+
+somatoria= n1+n2
+print('A somatoria dos números foram de {}'.format(somatoria))
