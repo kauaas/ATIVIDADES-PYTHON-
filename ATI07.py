97cb658cbc5cba6dd3c8970842190a322448b709
kauaas <57207681+kauaas@users.noreply.github.com>
2020-08-23 14:30:35 -0300

    Add files via upload

---
+++ ATI07.py
@@ -0,0 +1,4 @@
+##Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.
+f = float(input('Indique a sua temperatura: '))
+c = (fah-32)/1.8
+print('A temperatura de fahrenheit é {} a conversão para celsius é igual a: {}'.format(f,c))
