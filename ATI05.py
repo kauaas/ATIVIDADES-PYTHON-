626500da9751d7f6125d29a35d99e20c788d94a6
kauaas <57207681+kauaas@users.noreply.github.com>
2020-08-23 14:26:09 -0300

    Add files via upload

---
+++ ATI05.py
@@ -0,0 +1,5 @@
+##Faça um Programa que converta metros para centímetros
+print('Conversor de metro para centímetros')
+met = float(input('Informe os metros: '))
+cen= m*100
+print('A conversão de metros para centímetros é igual a: {}'.format(cen))
