4bfedaff556c00ceb28c108e028e900f548bf4bb
kauaas <57207681+kauaas@users.noreply.github.com>
2020-08-23 14:32:17 -0300

    Add files via upload

---
+++ ATIV08.py
@@ -0,0 +1,6 @@
+ 
+## Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
+c = float (input('Informe a temperatura em graus celsius: '))
+f = (c*1.8)+32
+print(' A temperatura em celsius é {} a conversão para fahrenheit deu como resultado {} '.format(c,f))
+
